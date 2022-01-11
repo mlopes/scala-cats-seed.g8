8b678cef31ba15e7db497b9bf15f4d726fff3168
Marco Lopes <marco@txodds.com>
2022-01-11 10:52:52 +0000

    Template for Scala 3.1.0

--- src/main/g8/build.sbt
+++ src/main/g8/build.sbt
@@ -1,10 +1,13 @@
-ThisBuild / scalaVersion     := "2.13.4"
+ThisBuild / useCoursier := true
+
+ThisBuild / scalaVersion     := "3.1.0"
 ThisBuild / version          := "0.1.0-SNAPSHOT"
 ThisBuild / organization     := "com.example"
 ThisBuild / organizationName := "example"
 
 lazy val root = (project in file("."))
   .settings(
+    scalaVersion := "3.1.0",
     name := "$name$",
     libraryDependencies ++= Dependencies.Cats.deps ++ Dependencies.Test.deps
   )
--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   object Test {
-    private val scalatestVersion = "3.2.3"
+    private val scalatestVersion = "3.2.10"
 
     val deps = Seq(
       "org.scalatest" %% "scalatest" % scalatestVersion  % "test"
@@ -11,8 +11,8 @@ object Dependencies {
   }
 
   object Cats {
-    private val catsEffectVersion = "2.3.0-M1"
-    private val catsVersion = "2.2.0"
+    private val catsEffectVersion = "3.3.4"
+    private val catsVersion = "2.7.0"
 
     val deps = Seq(
         "org.typelevel" %% "cats-effect" % catsEffectVersion
